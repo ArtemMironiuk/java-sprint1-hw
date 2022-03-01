8c93ba23322bdd8c98337bf1fc017f8383fed9e5
Артём Миронюк <a.mironyuk@global.local>
2022-03-01 14:28:16 +0300

    Создание классов StepTracker и Converter

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Поехали!
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+
+            if (command == 1){
+
+
+            } else if (command == 2){
+
+
+            } else if (command == 3) {
+
+
+            } else if (command == 4) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+
+        }
+
+
+    }
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Ввести количество шагов за определённый день");
+        System.out.println("2 - Напечатать статистику за определённый месяц");
+        System.out.println("3 - Изменить цель по количеству шагов в день");
+        System.out.println("4 - Выйти из приложения");
+
     }
 }
 
